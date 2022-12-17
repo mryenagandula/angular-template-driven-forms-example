4200e598f6b46cefa9abc95d18ed510e9993c297
SpringSrikanth <srikanthyenagagandula89@gmail.com>
2022-12-18 02:42:34 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
-
+interface User {
+  firstName:string,
+  middleName:string,
+  lastName:string,
+  gender:string,
+  mobile:string | number,
+  emailId:string,
+  username:string,
+  password: string,
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +16,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-reactive-forms-example';
+  userModel = {
+    firstName:'',
+    middleName:'',
+    lastName:'',
+    gender:'',
+    mobile:'',
+    username:'',
+    emailId:'',
+    password: '',
+  } as User;
+  constructor(){}
+  public submitForm(frm){
+    console.log(frm);  
+    console.log(frm.value);
+  }
 }
